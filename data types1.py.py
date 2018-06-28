5257e5126040f071e2bb8db460e17549ff792932
nikhil742 <40525171+nikhil742@users.noreply.github.com>
2018-06-28 13:13:52 +0530

    Add files via upload

---
+++ data types1.py.py
@@ -0,0 +1,39 @@
+#1)creating a list
+Animals=['tiger','elephant','deer','aneone']
+print(Animals)
+
+#2)add the following list
+names=['nikhil','rohit']
+apps=['google','apple','facebook','microsoft','tesla']
+print(names+apps)
+
+#3) creating a list
+l=['1','apple','2','orange','apple','orange','2','apple']
+#counting the number of time of occurrences of apple
+count=l.count('apple')
+print("the final count is",count)
+
+#4)creat a list in assending order
+var3=['9','5','6']
+print(sorted(var3))
+ 
+#5)creat a two list
+a=['5','7','1','4']
+b=['9','6','8','3']
+#merge them into a single sorted list
+c=sorted(a+b)
+print(c)
+
+#6)stack
+x=['1','3','5','6']
+x.pop(0)
+x.append(998)
+print(x)
+# queue
+queue =["Ram", "Tarun", "Asif", "John"]
+print(queue)
+queue.append("Akbar")
+print(queue)
+queue.append("Birbal")
+print(queue)
+                
