fa7a66711efe9a4397695a6f1f0b6cba790d1e4d
nikhil742 <40525171+nikhil742@users.noreply.github.com>
2018-06-28 13:11:36 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,26 @@
+#1)
+a='nikhil'
+print(a)
+#2)
+a='nik'
+b='hil'
+print(a+b)
+#3)
+x=input("22")
+y=input("enter a age")
+z=input("enter a name")
+print(x,y,z)
+ 
+#4)
+print("let's get started")
+
+#5)
+s="acadview"
+course="python"
+fees=5000
+print("i am doing a course from %s and my course name is %s,and fees is %d" %(s,course,fees))
+
+#6)
+name="tony stark"
+salary=100000
+print("%s salary is %d"%(name,salary))
