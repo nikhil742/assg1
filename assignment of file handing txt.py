0abec1f828248a1da0a53178a2fc8ad1f0a49b1d
nikhil sharma <40525171+nikhil742@users.noreply.github.com>
2018-07-04 11:25:39 +0530

    Add files via upload

---
+++ assignment of file handing txt.py
@@ -0,0 +1,37 @@
+#ques1-->
+f=open('xyz.txt','r',encoding='utf8')
+d=f.readlines()
+print("last line is-->",d[-1])
+
+#ques2-->
+with open("xyz.txt",'r',encoding='utf8') as t:
+    d=t.read().split()
+    print(len(d))
+
+#ques3-->
+with open("xyz.txt",encoding='utf8') as f:
+    with open("out.txt", "w",encoding='utf8') as f1:
+        for line in f:
+            f1.write(line)
+#ques4-->
+with open('xyz.txt',encoding='utf8') as fh1, open('out.txt',encoding='utf8') as fh2:
+    for line1, line2 in zip(fh1, fh2):
+        print(line1+line2)
+
+#ques5-->
+import random
+f=open('Random.txt','w')
+for i in range(10):
+    data=str(random.randint(1,10))
+    f.write(data)
+    f.write("\n")
+f.close()
+print("\nReading the file now")
+file1 = open("Random.txt",'r')
+file2 = open("sorted.txt",'w')
+list1=file1.readlines()
+list1.sort()
+file2.writelines(list1)
+file1.close()
+file2.close()
+
