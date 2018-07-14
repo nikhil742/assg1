52aa34cfa7dc54a768075fff4677657da946c64e
nikhil sharma <40525171+nikhil742@users.noreply.github.com>
2018-07-14 21:51:00 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -29,15 +29,15 @@ def clear():
  
 if __name__ == "__main__":
     
-    gui = Tk()
-    gui.configure(background="white")
-    gui.title("Calculator")
-    gui.geometry("305x180")
+    root = Tk()
+    root.configure(background="white")
+    root.title("Calculator")
+    root.geometry("305x180")
     
     equation = StringVar()
        
    
-    expression_field = Entry(gui, text=equation)
+    expression_field = Entry(root, text=equation)
  
     
     expression_field.grid(columnspan=4, ipadx=70)
@@ -46,69 +46,70 @@ if __name__ == "__main__":
      
 
     
-    button1 = Button(gui, text=' 1 ', fg='white', bg='grey',
+    button1 = Button(root, text=' 1 ', fg='white', bg='grey',
                      command=lambda: number(1), height=1, width=6, font = (15))
     button1.grid(row=4, column=0)
  
-    button2 = Button(gui, text=' 2 ', fg='white', bg='grey',
+    button2 = Button(root, text=' 2 ', fg='white', bg='grey',
                      command=lambda: number(2), height=1, width=6, font = (15))
     button2.grid(row=4, column=1)
  
-    button3 = Button(gui, text=' 3 ', fg='white', bg='grey',
+    button3 = Button(root, text=' 3 ', fg='white', bg='grey',
                      command=lambda: number(3), height=1, width=6, font = (15))
     button3.grid(row=4, column=2)
  
-    button4 = Button(gui, text=' 4 ', fg='white', bg='grey',
+    button4 = Button(root, text=' 4 ', fg='white', bg='grey',
                      command=lambda: number(4), height=1, width=6, font = (15))
     button4.grid(row=3, column=0)
  
-    button5 = Button(gui, text=' 5 ', fg='white', bg='grey',
+    button5 = Button(root, text=' 5 ', fg='white', bg='grey',
                      command=lambda: number(5), height=1, width=6, font = (15))
     button5.grid(row=3, column=1)
  
-    button6 = Button(gui, text=' 6 ', fg='white', bg='grey',
+    button6 = Button(root, text=' 6 ', fg='white', bg='grey',
                      command=lambda: number(6), height=1, width=6, font = (15))
     button6.grid(row=3, column=2)
  
-    button7 = Button(gui, text=' 7 ', fg='white', bg='grey',
+    button7 = Button(root, text=' 7 ', fg='white', bg='grey',
                      command=lambda: number(7), height=1, width=6, font = (15))
     button7.grid(row=2, column=0)
  
-    button8 = Button(gui, text=' 8 ', fg='white', bg='grey',
+    button8 = Button(root, text=' 8 ', fg='white', bg='grey',
                      command=lambda: number(8), height=1, width=6, font = (15))
     button8.grid(row=2, column=1)
  
-    button9 = Button(gui, text=' 9 ', fg='white', bg='grey',
+    button9 = Button(root, text=' 9 ', fg='white', bg='grey',
                      command=lambda: number(9), height=1, width=6, font = (15))
     button9.grid(row=2, column=2)
  
-    button0 = Button(gui, text=' 0 ', fg='white', bg='grey',
+    button0 = Button(root, text=' 0 ', fg='white', bg='grey',
                      command=lambda: number(0), height=1, width=6, font = (15))
     button0.grid(row=5, column=0)
  
-    plus = Button(gui, text=' + ', fg='white', bg='grey',
+    plus = Button(root, text=' + ', fg='white', bg='grey',
                    command=lambda: number("+"), height=1, width=6,  font =(15))
     plus.grid(row=2, column=3)
  
-    minus = Button(gui, text=' - ', fg='white', bg='grey',
+    minus = Button(root, text=' - ', fg='white', bg='grey',
                    command=lambda: number("-"), height=1, width=6, font = (15))
     minus.grid(row=3, column=3)
  
-    multiply = Button(gui, text=' * ', fg='white', bg='grey',
+    multiply = Button(root, text=' * ', fg='white', bg='grey',
                    command=lambda: number("*"), height=1, width=6, font = (15))
     multiply.grid(row=4, column=3)
  
-    divide = Button(gui, text=' / ', fg='white', bg='grey',
+    divide = Button(root, text=' / ', fg='white', bg='grey',
                     command=lambda: number("/"), height=1, width=6, font = (15))
     divide.grid(row=5, column=3)
  
-    equal = Button(gui, text=' = ', fg='white', bg='grey',
+    equal = Button(root, text=' = ', fg='white', bg='grey',
                    command=equal, height=1, width=6, font = (15))
     equal.grid(row=5, column=2)
  
-    clear = Button(gui, text='Clear', fg='white', bg='grey',
+    clear = Button(root, text='Clear', fg='white', bg='grey',
                    command=clear, height=1, width=6, font = (15))
     clear.grid(row=5, column='1')
  
     
-    gui.mainloop()
+    root.mainloop()
+
